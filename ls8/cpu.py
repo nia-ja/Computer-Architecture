fe8ac81f988e8b1581e457bccc463650aa5194f4
Kasia Bondarava <kasia.bondarava@gmail.com>
2020-05-12 20:16:15 -0400

    More fixes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,25 +12,33 @@ class CPU:
         self.reg = [0] * 8      #registers
         self.pc = 0             #program counter/current instruction
         self.ir = 0             #instruction register/currently executing instruction
-        self.halted = False
+        # self.sp = 7                 #location of stack pointer in registers
+        # self.reg[self.sp] = 0xF4    #initialize stack pointer
+        self.halted = False #interrupt status
         self.instruction = {    #cpu instruction set and their methods
             0b00000001: self.HLT,
             0b10000010: self.LDI,
             0b01000111: self.PRN,
             0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP,
+            0b01010000: self.CALL,
+            0b00010001: self.RET,
+            0b10100000: self.ADD,
         }
 
-    def load(self, program=[]):
+    def load(self, program):
         """Load a program into memory."""
         #strip out all function instructions
         instructions = []
         for line in program:
             line = line.strip()
-            # take only bytecode, not a comment
+            # ignore everything after a #, since that's a comment
             instruction = line.split('#')[0]
             # print(instruction)
             if instruction == '':
                 continue
+            # convert the binary strings to integer values to store in RAM
             # int(x, base)
             # x - a number or string to be converted to integer object
             # base - Number format. Default value: 10
@@ -97,6 +105,24 @@ class CPU:
         reg_b = self.ram_read(self.pc+2)
         self.alu('MUL', reg_a, reg_b)
         self.pc += 3
+    def PUSH(self, MDR=None):
+        self.reg[7] -= 1
+        data = MDR if MDR else self.reg[self.ram[self.pc+1]]
+        self.ram_write(self.reg[7], data)
+        self.pc += 2
+    def POP(self):
+        self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.reg[7])
+        self.pc += 2
+        self.reg[7] += 1
+    def ADD(self):
+        self.alu('ADD', self.ram_read(self.pc+1),self.ram_read(self.pc+2))
+        self.pc += 3
+    def CALL(self):
+        self.PUSH(self.pc+2)
+        self.pc = self.reg[self.ram_read(self.pc-1)]
+    def RET(self):
+        self.pc = self.ram_read(self.reg[7])
+        self.reg[7] += 1
 
     def trace(self):
         """
